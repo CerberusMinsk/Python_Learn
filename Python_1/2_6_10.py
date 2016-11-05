f990f434b31112b698513cc050d0c944c48ac548
Cerberus <cerberus.minsk@gmail.com>
2016-11-05 14:40:04 +0300

    input and output finished

--- Python_1/2_6_10.py
+++ Python_1/2_6_10.py
@@ -1,1 +1,19 @@
-print('2_6_10')
+#('2_6_10')
+mtrx = []
+inpt = True
+lst = []
+while inpt:
+    lst = (input().split())
+    if lst != ['end']:
+        mtrx.append(lst)
+    else:
+        break
+
+row = len(mtrx)
+col = len(mtrx[0])
+#print(row)
+#print(col)
+for i in range(row):
+    for j in range(col):
+        print(mtrx[i][j], end=' ')
+    print()
