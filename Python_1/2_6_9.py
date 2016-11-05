fe761f6780c95f81de6fc80ed4be9da239b1256e
Cerberus <cerberus.minsk@gmail.com>
2016-11-05 11:07:53 +0300

    ADD 2_6_9

---
+++ Python_1/2_6_9.py
@@ -0,0 +1,11 @@
+lst = [i for i in input().split()]
+x = input()
+
+cnt = True
+g = 0
+for i in range(len(lst)):
+	if lst[i] == x:
+		print(i, end=' ')
+		cnt = False
+if cnt:
+	print('Отсутствует')
