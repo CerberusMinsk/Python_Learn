ee3614bf5b284ea0ff5870300249f738921ece94
Cerberus <cerberus.minsk@gmail.com>
2016-11-07 00:01:57 +0300

    2_6_10 finished

--- Python_1/2_6_10.py
+++ Python_1/2_6_10.py
@@ -1,7 +1,9 @@
 #('2_6_10')
 mtrx = []
+mtrx_2 = []
 inpt = True
 lst = []
+a = 0
 while inpt:
     lst = (input().split())
     if lst != ['end']:
@@ -13,7 +15,24 @@ row = len(mtrx)
 col = len(mtrx[0])
 #print(row)
 #print(col)
+mtrx_2 = [[0 for j in range(col)] for i in range(row)]
+
+for i in range(row):
+    if (i + 1) >= row:
+        i_r = -1
+    else:
+        i_r = i
+    for j in range(col):
+        if (j + 1) >= col:
+            j_r = -1
+        else:
+            j_r = j
+        mtrx_2[i][j] = int(mtrx[i][j - 1]) + int(mtrx[i - 1][j])
+        mtrx_2[i][j] += int(mtrx[i][j_r + 1]) + int(mtrx[i_r + 1][j])
+
+
+
 for i in range(row):
     for j in range(col):
-        print(mtrx[i][j], end=' ')
+        print(mtrx_2[i][j], end=' ')
     print()
--- testing.py
+++ testing.py
@@ -1,4 +1,8 @@
-
-
-print('dkfjksdjfksd')
-print('dddddddddddddd')
+col = 3
+row = 4
+mtrx_2 = [[0 for j in range(col)] for i in range(row)]
+print(mtrx_2)
+for i in range(row):
+    for j in range(col):
+        print(mtrx_2[i][j], end=' ')
+    print()
