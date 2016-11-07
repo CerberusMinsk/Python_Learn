a7972dcf0e7a39405d4b3dd1a9ff6b4c86b79d7d
MURic <murashka@alexey.by>
2016-11-07 10:56:50 +0300

    2 rows completed

--- Python_1/2_6_11.py
+++ Python_1/2_6_11.py
@@ -3,15 +3,22 @@ mtrx = [[0 for j in range(n)] for i in range(n)]
 num = 1
 x = 0
 y = 0
-y_end = n
-x_end = n
+y_end = 0
+x_end = 0
 kfc = 1
 while num <= n ** 2:
 
-    while y < x_end:
+    while y < n:
         mtrx[x][y] = num
         num += 4
-        y += kfc
+        y += 1
+
+    x_end += 1
+    y -= 1
+    while x_end < n:
+        mtrx[x_end][y] = num
+        num += 4
+        x_end += 1
 
 
 
--- testing.py
+++ testing.py
@@ -1,10 +1,16 @@
-col = 3
+col = 4
 row = 4
 mtrx_2 = [[0 for j in range(col)] for i in range(row)]
 print(mtrx_2)
-mtrx_2 [1][2] = 1
+print()
+mtrx_2 [3][0] = 8
 print(mtrx_2)
+print()
 for i in range(row):
     for j in range(col):
         print(mtrx_2[i][j], end=' ')
     print()
+
+print()
+for i in range(9, 3, -1):
+    print(i, end=' ')
