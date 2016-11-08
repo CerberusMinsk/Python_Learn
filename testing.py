de8683eca7a59b428a0932c5d75691dca2393c0e
MURic <murashka@alexey.by>
2016-11-08 13:36:49 +0300

    testing

--- testing.py
+++ testing.py
@@ -1,9 +1,10 @@
-col = 4
-row = 4
+col = 5
+row = 5
 mtrx_2 = [[0 for j in range(col)] for i in range(row)]
 print(mtrx_2)
 print()
-mtrx_2 [3][0] = 8
+mtrx_2 [2][1] = 1
+mtrx_2 [2][2] = 9
 print(mtrx_2)
 print()
 for i in range(row):
