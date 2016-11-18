b950f4dd2ebc4a8712b7a9e290c1557602bdd25b
Cerberus <cerberus.minsk@gmail.com>
2016-11-19 01:57:17 +0300

    completed

---
+++ Python_1/3_6_2.py
@@ -0,0 +1,10 @@
+import requests
+cnt = 0
+with open('dataset_3378_2.txt', 'r') as inf:
+    url = inf.read().strip()
+print(url)
+r = requests.get(url)
+
+lst = r.text.splitlines()
+print(len(lst))
+#print(r.text)
