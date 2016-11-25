7414c68b0214caaae8ce5a4b7ed9c190d4c41185
Cerberus <cerberus.minsk@gmail.com>
2016-11-25 09:29:35 +0300

    recursion

--- testing.py
+++ testing.py
@@ -1,17 +1,7 @@
-col = 5
-row = 5
-mtrx_2 = [[0 for j in range(col)] for i in range(row)]
-print(mtrx_2)
-print()
-mtrx_2 [2][1] = 1
-mtrx_2 [2][2] = 9
-print(mtrx_2)
-print()
-for i in range(row):
-    for j in range(col):
-        print(mtrx_2[i][j], end=' ')
-    print()
+def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x - 1) + fib(x - 2)
 
-print()
-for i in range(9, 3, -1):
-    print(i, end=' ')
+print(fib(4))
