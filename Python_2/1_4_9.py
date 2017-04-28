de2c8aa66b1ce66246047a08ea5183a89d6e2be0
Cerberus <cerberus.minsk@gmail.com>
2017-04-28 19:17:17 +0300

    py2 1.4.9 add def add()

--- Python_2/1_4_9.py
+++ Python_2/1_4_9.py
@@ -1,7 +1,18 @@
-i = input()
+glb = []
+
+def add (nspc, var):
+    print(nspc, var)
+    if nspc == 'global':
+        glb.append(var)
+    else:
+        pass
+    return 0
+
+i = input().split()
+
+if i[0] == 'add':
+    print('add')
+    add(i[1],i[2])
 
 
-
-
-
-print(i)
+print(glb)
