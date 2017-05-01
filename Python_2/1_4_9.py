2cffc6ebeb3598d7b1f57505cc99268003fd3de3
Cerberus <cerberus.minsk@gmail.com>
2017-05-01 16:28:15 +0300

    Failed test #2. Wrong answer

--- Python_2/1_4_9.py
+++ Python_2/1_4_9.py
@@ -10,20 +10,18 @@ def create (namespace, parent):
     glb.append({'parent':parent, 'namespace':namespace, 'var':''})
 
 def get (namespace, var):
-   for i in glb:
-       if namespace == i.get('namespace'):
-           if var == i.get('var'):
-               print(i.get('namespace'))
-               break
-           elif i.get('parent') == 'None':
-               if var != i.get('var'):
-                   if a[1] == 'global':
-                       print('global')
-                   else:
-                       print('None')
-
-           else:
-               get(i.get('parent'), var)
+  for i in range(len(glb)):
+    if namespace == glb[i].get('namespace'):
+      temp = glb[i]
+  if namespace == temp.get('namespace'):
+    if var == temp.get('var'):
+      print(temp.get('namespace'))
+      return 0
+    elif temp.get('parent') == 'None' and var != temp.get('var'):
+      print('None')
+      return 0
+    else:
+      return get(temp.get('parent'), var)
 
 n = int(input())
 
