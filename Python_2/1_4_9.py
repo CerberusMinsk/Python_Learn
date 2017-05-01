78bbe2c1724f75cda2a7a0c416ff317617b87d9c
Cerberus <cerberus.minsk@gmail.com>
2017-05-01 23:30:39 +0300

    PEP8

--- Python_2/1_4_9.py
+++ Python_2/1_4_9.py
@@ -1,27 +1,31 @@
-glb = [{'parent':'None', 'namespace':'global', 'var':''}]
+glb = [{'parent': 'None', 'namespace': 'global', 'var': ''}]
 
-def add (namespace, var):
+
+def add(namespace, var):
     for i in glb:
         if namespace == i.get('namespace'):
             i['var'] = var
-            #print(i.get('var'))
+            # print(i.get('var'))
 
-def create (namespace, parent):
-    glb.append({'parent':parent, 'namespace':namespace, 'var':''})
 
-def get (namespace, var):
-  for i in range(len(glb)):
-    if namespace == glb[i].get('namespace'):
-      temp = glb[i]
-  if namespace == temp.get('namespace'):
-    if var == temp.get('var'):
-      print(temp.get('namespace'))
-      return 0
-    elif temp.get('parent') == 'None' and var != temp.get('var'):
-      print('None')
-      return 0
-    else:
-      return get(temp.get('parent'), var)
+def create(namespace, parent):
+    glb.append({'parent': parent, 'namespace': namespace, 'var': ''})
+
+
+def get(namespace, var):
+    for i in range(len(glb)):
+        if namespace == glb[i].get('namespace'):
+            temp = glb[i]
+    if namespace == temp.get('namespace'):
+        if var == temp.get('var'):
+            print(temp.get('namespace'))
+            return 0
+        elif temp.get('parent') == 'None' and var != temp.get('var'):
+            print('None')
+            return 0
+        else:
+            return get(temp.get('parent'), var)
+
 
 n = int(input())
 
