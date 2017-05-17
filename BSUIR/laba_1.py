c2802ee849b8b76899172d19ccb92d709210d248
Cerberus <cerberus.minsk@gmail.com>
2017-05-18 00:04:21 +0300

    procces 2

--- BSUIR/laba_1.py
+++ BSUIR/laba_1.py
@@ -2,20 +2,31 @@ import os
 import re
 import shutil
 
+
+sss = 1
+
 def coppy(dir):
     print(dir)
 
 
-def walk(dir, dir_copy):
-  for name in os.listdir(dir):
-    path = os.path.join(dir, name)
-    path_copy = os.path.join(dir_copy, name)
-    if os.path.isfile(path):
-        shutil.copyfile(path, path_copy)
-        coppy(name)
 
-    else:
-        walk(path, dir_copy)
+def walk(dir, dir_copy):
+    global sss
+    for name in os.listdir(dir):
+        result = re.findall(r'\.txt', name)
+        fln = '4_' + str(sss) + '.html'
+        path = os.path.join(dir, name)
+        path_copy = os.path.join(dir_copy, fln)
+        if os.path.isfile(path):
+            if result:
+                shutil.copyfile(path, path_copy)
+                sss = sss + 1
+                print(sss)
+                #coppy(name)
+
+        else:
+            walk(path, dir_copy)
+
 
 # os.rename(r"ttt.py", r'test1.py') #переименование файла
 path = r'/home/cerberus'
@@ -27,11 +38,4 @@ fullpath = os.path.join(path, folder)
 if not os.path.exists(fullpath):
     os.mkdir(fullpath)
 
-
 walk(project_dir, fullpath)
-
-
-
-
-
-
