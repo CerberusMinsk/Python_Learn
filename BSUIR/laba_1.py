5a5a49a2274afc216decb087a54716078b44fb55
Cerberus <cerberus.minsk@gmail.com>
2017-05-18 22:04:30 +0300

    Laba 1 complite

--- BSUIR/laba_1.py
+++ BSUIR/laba_1.py
@@ -2,40 +2,54 @@ import os
 import re
 import shutil
 
-
-sss = 1
-
-def coppy(dir):
-    print(dir)
-
+count = 1
 
 
 def walk(dir, dir_copy):
-    global sss
+    global count
     for name in os.listdir(dir):
         result = re.findall(r'\.txt', name)
-        fln = '4_' + str(sss) + '.html'
+        fln = '4_' + str(count) + '.html'
         path = os.path.join(dir, name)
         path_copy = os.path.join(dir_copy, fln)
         if os.path.isfile(path):
             if result:
                 shutil.copyfile(path, path_copy)
-                sss = sss + 1
-                print(sss)
-                #coppy(name)
-
+                count = count + 1
         else:
             walk(path, dir_copy)
 
 
-# os.rename(r"ttt.py", r'test1.py') #переименование файла
 path = r'/home/cerberus'
 folder = 'LABDIR'
 
-project_dir = input('DIR name: ')
+project_dir = '/home/cerberus/LABDIR_COPY'  # input('DIR name: ')
 
 fullpath = os.path.join(path, folder)
 if not os.path.exists(fullpath):
     os.mkdir(fullpath)
 
 walk(project_dir, fullpath)
+lst = []
+
+for name in os.listdir(fullpath):
+    openpath = os.path.join(fullpath, name)
+    lst.append(openpath)
+
+for i in lst:
+    with open(i, 'r') as inf:
+        line_list = []
+        line_list.append('<html><body>\n')
+        for line in inf:
+            for word in line.split():
+                if re.match('ee', word):
+                    new_line = word.replace('ee', '22', 1) + ' '
+                else:
+                    new_line = word + ' '
+                line_list.append(new_line)
+            line_list.append('\n')
+        line_list.append('</html></body>\n')
+
+    with open(i, 'w') as ouf:
+        for i in line_list:
+            ouf.write(i)
