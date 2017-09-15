65b33f6f2486cc10084fbe8173143181e573553e
Cerberus <cerberus.minsk@gmail.com>
2017-09-15 18:04:22 +0300

    Complete 2.1.6

---
+++ Python_2/2_1_6.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+try:
+    foo()
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+except ArithmeticError:
+    print('ArithmeticError')
+except AssertionError:
+    print('AssertionError')
